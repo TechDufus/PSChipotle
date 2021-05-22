92c3cac8f4603b1575db373dc3430f5a26146917
Matthew J. DeGarmo <matthewjdegarmo@gmail.com>
2021-05-21 20:52:30 -0500

    Adding base function scaffolding.

---
+++ Functions/Public/Get-ChipotleAccount.ps1
@@ -0,0 +1,28 @@
+#Region Get-ChipotleAccount
+
+<#
+.SYNOPSIS
+    Get Chipotle account information given credentials.
+.DESCRIPTION
+.NOTES
+    Author: matthewjdegarmo
+    GitHub: https://github.com/matthewjdegarmo
+#>
+Function Get-ChipotleAccount() {
+    [CmdletBinding()]
+    Param()
+
+    Begin {}
+
+    Process {
+        Try {
+
+        } Catch {
+
+        }
+    }
+
+    End {}
+}
+
+#EndRegion Get-ChipotleAccount
---
+++ Functions/Public/Get-ChipotleMeals.ps1
@@ -0,0 +1,31 @@
+#Region Get-ChipotleMeals
+
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.NOTES
+    Author: matthewjdegarmo
+    GitHub: https://github.com/matthewjdegarmo
+    API IRU: https://lt-services.chipotle.com/menuinnovation/v1/restaurants/{restaurantId}/onlinemeals[?channelId][&includeUnavailableItems]
+.LINK
+    https://lt-servicesportal.chipotle.com/docs/services/3a753a58f40945adb56ce4b621b9d6f6/operations/GetOnlineMealsByRestaurantId
+#>
+Function Get-ChipotleMeals() {
+    [CmdletBinding()]
+    Param()
+
+    Begin {}
+
+    Process {
+        Try {
+
+        } Catch {
+            Throw $_
+        }
+    }
+
+    End {}
+}
+#EndRegion Get-ChipotleMeals
